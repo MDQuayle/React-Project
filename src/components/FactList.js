f4f65086c6fbd56e3bb9f5781f98b501e10d5c5a
mdquayle <misternopants@gmail.com>
2021-09-01 17:45:07 -0400

    Names components and initializes their code

---
+++ src/components/FactList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FactCard from './components/FactCard';
+
+function FactList(){
+    return (
+        <div>
+            <FactCard />
+        </div>
+    )
+}
+
+export default FactList; 
