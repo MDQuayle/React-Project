8d5470dc361cfb1c1ca3ecf77dc63456c69a104b
mdquayle <misternopants@gmail.com>
2021-09-07 17:21:14 -0400

    fixes fetch call website

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const [facts,setFacts] = useState([])
 
 
 useEffect(() => {
-  fetch('localhost:3000')
+  fetch('http://localhost:3000/facts')
   .then(res=> res.json())
   .then(factData => setFacts(factData))
 },[])
