911e8a70f67cbe559d5b0312e3218acb2fbbc628
mdquayle <misternopants@gmail.com>
2021-09-12 08:49:00 -0400

    adds alt prop to img tag in FactCard

--- src/components/FactCard.js
+++ src/components/FactCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 function FactCard({fact}){
     return (
         <div>
-            <img src= {fact.image}/>
+            <img src= {fact.image} alt="animal"/>
             <h2>{fact.animal}</h2>
             <p><span>Fact:</span> {fact.factoid}</p>
             
