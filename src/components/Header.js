9654905fb4293f60af2388f6aef20ccd1e3d7ad5
mdquayle <misternopants@gmail.com>
2021-09-22 17:29:10 -0400

    changes title for internet tab, adds text to the header, slightly edits home text

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,9 @@ import Navbar from "./Navbar";
 function Header(){
     return (
         <header>
+            <div>
+                <h1>Animal Facts</h1>
+            </div>
         <Navbar />
         </header>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home(){
     return (
         <div>
-            <h1>Welcome to Animal Facts!</h1>
+            <h1>Welcome to Animal Facts</h1>
             <h4>Check out our facts or add your own!</h4>
         </div>
     )
